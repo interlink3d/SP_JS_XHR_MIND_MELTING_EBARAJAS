78d77e956ea3984455e00f3f57baa90311aae934
Edgar Barajas <ebarajas05@gmail.com>
2016-07-27 10:20:04 -0500

    completed step 5 and part of 6

--- iife-xhr.js
+++ iife-xhr.js
@@ -1,1 +1,19 @@
-console.log("hello world")
+// step 6 and 7
+
+function showCarnivores (carnivores) {
+  var list document.querySelector('.carnivores')
+
+  carnivores.forEach(function (carnivore) {
+    list.innerHTML += `<li>carnivore</li>`
+  })
+}
+
+function showHerbivores () {
+  var list document.querySelector('.herbivores')
+
+  herbivores.forEach(function (herbivore) {
+    list.innerHTML += `<li>herbivore</li>`
+}
+
+Predator.getCarnivores(showCarnivores);
+Predator.getHerbivores(showHerbivores);
